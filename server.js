f14c37f034e532d2aca709fe43440e53f3f25576
alex nichols <alex nichols>
2017-04-23 20:26:18 -0700

    server created

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+
+//creating our express server
+var app = express();
+
+var port = 8080;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burger_controllers.js");
+
+//starting our server and listening on port 8080
+app.listen(port);
