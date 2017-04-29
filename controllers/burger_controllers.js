00cd8231d116257c245dbf6e404d21a5031b34e5
alex nichols <alex nichols>
2017-04-28 22:09:09 -0700

    minor app updates

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -21,7 +21,7 @@ router.get("/", function(req, res){
 	});
 });
 
-router.put("/:id", function(req, res){
+router.post("/:id", function(req, res){
 	var condition = "id = " + req.params.id;
 
 	burger.update(req.body.devoured, condition, function(){
